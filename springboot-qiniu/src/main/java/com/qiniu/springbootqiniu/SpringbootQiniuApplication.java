7d816191ccf403326cd07e782d7cb1dd9964ca62
yijun <134065253+gaoqinqinshan@users.noreply.github.com>
2024-10-26 21:31:39 +0800

    Add files via upload

---
+++ springboot-qiniu/src/main/java/com/qiniu/springbootqiniu/SpringbootQiniuApplication.java
@@ -0,0 +1,13 @@
+package com.qiniu.springbootqiniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQiniuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQiniuApplication.class, args);
+    }
+
+}
---
+++ springboot-qiniu/src/test/java/com/qiniu/springbootqiniu/SpringbootQiniuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiniu.springbootqiniu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQiniuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
